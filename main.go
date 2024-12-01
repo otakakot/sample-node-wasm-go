2d5accf530ca139e99676caf9b27637ae24b939e
otakakot <kotaro.otaka@gmail.com>
2024-12-01 09:59:46 +0900

    first commit

--- main.go
+++ main.go
@@ -5,18 +5,19 @@ import "syscall/js"
 var done = make(chan struct{})
 
 func init() {
-	js.Global().Set("golog", js.FuncOf(func(_ js.Value, args []js.Value) interface{} {
+	js.Global().Set("golog", js.FuncOf(func(_ js.Value, args []js.Value) any {
 		defer func() {
 			done <- struct{}{}
 		}()
 
 		println(args[0].String())
 
+		js.Global().Get("console").Call("log", args[0].String())
+
 		return nil
 	}))
 }
 
 func main() {
-	js.Global().Get("console").Call("log", "Hello, WebAssembly!")
 	<-done
 }
